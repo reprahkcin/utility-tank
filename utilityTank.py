26d4ac867d9e4a317abd657f8c920655a7ecabac
reprahkcin <bignickharper@gmail.com>
2021-05-14 22:46:16 -0700

    roughing shit out

--- utilityTank.py
+++ utilityTank.py
@@ -1,1 +1,39 @@
-print('loaded.')
+from time import sleep
+
+# joystick
+
+UP = "up"
+DOWN = "down"
+LEFT = "left"
+RIGHT = "right"
+
+Y = "Y"
+X = "X"
+B = "B"
+A = "A"
+
+SELECT = "select"
+START = "start"
+
+REFRESH_RATE = 0.05
+
+# Behaviors
+
+def turnRight():
+    print('Turning Right')
+
+def turnLeft():
+    print('Turning Left')
+
+def forward():
+    print('Moving Forward')
+
+def reverse():
+    print('Moving Backward')
+
+while(True):
+    # check the d-pad
+    # check the buttons
+    
+    print('checking...')
+    sleep(REFRESH_RATE)
